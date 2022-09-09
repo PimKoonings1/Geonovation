280d899f065ca302e3e28bbc1e6ed7ba1f561503
PimKoonings1 <66310029+PimKoonings1@users.noreply.github.com>
2022-09-09 10:51:20 +0200

    1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     echo "Hello Geonovation!";
     $content = file_get_contents('Images/download.jpg');
-    header('Content-Type: image/jpg');
+    header('Content-Type: image/jpeg');
     echo $content;
 
 
