6cb4c7108a1242baba7289291b3cbf9fc9085fe9
PimKoonings1 <66310029+PimKoonings1@users.noreply.github.com>
2022-09-09 11:08:27 +0200

    jpg

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="Images/download.jpg" alt="">
 <?php
-    $content = file_get_contents('Images/download.jpg');
-    header('Content-Type: image/jpeg');
-    echo $content;
-
+    // $content = file_get_contents('Images/download.jpg');
+    // header('Content-Type: image/jpeg');
+    // echo $content;
+    echo "Hello Geonovation";
 ?>
+</body>
+</html>
+
+
